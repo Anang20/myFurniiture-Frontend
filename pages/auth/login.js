ba603cfab92def10b23837227665f8e3115f6ac9
Anang20 <anangsyah766@gmail.com>
2022-07-31 00:26:56 +0700

    FIX: menyambungkan API auth dari backend

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,10 +1,64 @@
 import Image from "next/image";
-import imageLogin from '../public/images/login.png'
+import imageLogin from '../../public/images/login.png'
 import 'bootstrap/dist/css/bootstrap.css';
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import Link from "next/link";
+import { useState, Fragment } from "react";
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/router";
+import jwtDecode from "jwt-decode";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [token, setToken] = useState('')
+    const onChangeEmail = (e) => {
+        const value = e.target.value
+        setEmail(value)
+    }
+
+    const onChangePassword = (e) => {
+        const value = e.target.value
+        setPassword(value)
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const router = useRouter()
+    const loginSubmit = async () => {
+
+    try {
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        console.log(formData)
+        axios.post("http://localhost:3222/auth/login", formData, {
+            headers: { 'content-type': 'application/json' }
+        }).then(result => {
+            
+
+            const decode = jwtDecode(result.data.accessToken)
+            const role = decode.query.role["role_name"];
+            window.alert(result.data.message)
+            if (role === "admin") {
+                window.alert("berhasil")
+                localStorage.setItem('accesToken', result.data.accessToken)
+                router.push("../dashboard")
+            } else if (role === "customer") {
+                localStorage.setItem('accessToken', result.data.accessToken)
+                router.push("/")
+            }
+        })
+    } catch (error) {
+        console.error(error);
+    } 
+}
+
     return (
         <>
         <div className="container">
@@ -22,17 +76,15 @@ const Login = () => {
                                             <div className="text-center">
                                                 <h1 className="h4 text-gray-900 mb-2" style={{ fontWeight: 'bold', fontSize: 36 }}>Masuk</h1>
                                             </div>
-                                            <form action="" method="POST">
+                                            <form onSubmit={onFormSubmit} method="POST">
                                                 <div className="form-group mt-4">
-                                                    <input type="email" style={{ borderRadius: 15 }} name="email" className="form-control form-control-email" placeholder="Masukkan Email..." required/>
+                                                    <input type="email" style={{ borderRadius: 15 }} name="email" className="form-control form-control-email" value={email} onChange={onChangeEmail} placeholder="Masukkan Email..." required/>
                                                 </div>
                                                 <div className="form-group mt-4">
-                                                    <input type="password" style={{ borderRadius: 15 }} name="password" className="form-control form-control-password" placeholder="Masukkan Password..." required/>
+                                                    <input type="password" style={{ borderRadius: 15 }} name="password" className="form-control form-control-password" value={password} onChange={onChangePassword} placeholder="Masukkan Password..." required/>
                                                 </div>
                                                 <div className="d-grid gap-2">
-                                                    <Link href="./dashboard/">
-                                                        <button type="submit" style={{ borderRadius: 15, backgroundColor: '#00B8B0', border: 'none' }} className="btn btn-success btn-user btn-block mt-4">Masuk</button>
-                                                    </Link>
+                                                    <button type="submit" style={{ borderRadius: 15, backgroundColor: '#00B8B0', border: 'none' }} onClick={loginSubmit} className="btn btn-success btn-user btn-block mt-4">Masuk</button>
                                                     <center>
                                                         <p>Belum Punya Akun?
                                                             <Link href="./register">
---
+++ pages/auth/register.js
@@ -0,0 +1,128 @@
+import Image from "next/image";
+import imageRegister from '../../public/images/register.png'
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../../styles/Home.module.css'
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router"
+import axios from "axios";
+
+const Register = () => {
+
+    const [nama_lengkap, setNamaLengkap] = useState([])
+    const [email, setEmail] = useState([])
+    const [no_telp, setNotlp] = useState([])
+    const [password, setPassword] = useState([])
+    const [roleId, setRoleid] = useState(2)
+
+    const onChangeNamaLengkap = (e) => {
+        const value = e.target.value
+        setNamaLengkap(value)
+    }
+
+    const onChangeEmail = (e) => {
+        const value = e.target.value
+        setEmail(value)
+    }
+
+    const onChangeNoTelephon = (e) => {
+        const value = e.target.value
+        setNotlp(value)
+    }
+
+    const onChangePassword = (e) => {
+        const value = e.target.value
+        setPassword(value)
+    }
+
+    const onChangeRoleid = (e) => {
+        const value = e.target.value
+        setRoleid(value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const router = useRouter()
+
+    const registerSubmit = async () => {
+
+        try {
+            setNamaLengkap(nama_lengkap)
+            setEmail(email)
+            setNotlp(no_telp)
+            setPassword(password)
+            const res = await axios.post("http://localhost:3222/auth/register", {nama_lengkap, email, no_telp, password})
+            .then(result => {
+                console.log(result.data.statusCode)
+                if (result.data.statusCode == 201 || result.data.statusCode == 200) {
+                    window.alert(result.data.message)
+                    router.push('/auth/login')
+                } else {
+                    window.alert("Gagal mendaftar")
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+
+    }
+
+    return (
+        <>
+        <div className="container">
+            <div className={styles.container}>
+                <div className="row justify-content-center mt-3">
+                    <div className="col-xl-10 col-lg-12 col-md-9">
+                        <div className="card o-hidden border-0 shadow-lg my-5">
+                            <div className="card-body p-0">
+                                <div className="row">
+                                    <div className="col-lg-5 d-none d-lg-block p-5">
+                                        <Image src={imageRegister} style={{ marginLeft: 35 }} width={650} height={700} alt={"register"}/>
+                                    </div>
+                                    <div className="col-lg-7">
+                                        <div className="p-5">
+                                            <div className="text-center">
+                                                <h1 className="h4 text-gray-900 mb-2" style={{ fontWeight: 'bold', fontSize: 36 }}>Daftar</h1>
+                                            </div>
+                                            <form onSubmit={onSubmit} method="POST">
+                                                <div className="form-group mt-4">
+                                                    <input type="text" style={{ borderRadius: 15 }} className="form-control" value={nama_lengkap} onChange={onChangeNamaLengkap} placeholder="Masukkan Nama Lengkap..." required/>
+                                                </div>
+                                                <div className="form-group mt-4">
+                                                    <input type="email" style={{ borderRadius: 15 }} className="form-control" value={email} onChange={onChangeEmail} placeholder="Masukkan Email..." required/>
+                                                </div>
+                                                <div className="form-group mt-4">
+                                                    <input type="number" style={{ borderRadius: 15 }} className="form-control" value={no_telp} onChange={onChangeNoTelephon} placeholder="Masukkan Nomor Telephone..." required/>
+                                                </div>
+                                                <div className="form-group mt-4">
+                                                    <input type="password" style={{ borderRadius: 15 }} className="form-control" value={password} onChange={onChangePassword} placeholder="Masukkan Password..." required/>
+                                                </div>
+                                                <div className="form-group mt-4">
+                                                    <input type="hidden" style={{ borderRadius: 15 }} className="form-control" value={roleId} onChange={onChangeRoleid} required/>
+                                                </div>
+                                                <div className="d-grid gap-2">
+                                                    <button type="submit" style={{ borderRadius: 15, backgroundColor: '#00B8B0', border: 'none' }} className="btn btn-success btn-user btn-block mt-4" onClick={registerSubmit}>Daftar</button>
+                                                    <center>
+                                                        <p>Sudah Punya Akun?
+                                                            <Link href="./login">
+                                                                <a style={{ color: '#00B8B0', textDecoration: 'none' }}> Masuk</a>
+                                                            </Link>
+                                                        </p>
+                                                    </center>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Register;
