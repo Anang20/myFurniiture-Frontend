f7c1bef1d9c86c4ddf09574f14aed6b9c57e99c3
Anang20 <anangsyah766@gmail.com>
2022-08-03 11:50:46 +0700

    FEAT: tambah halaman alamat & edit profil admin

--- pages/dashboard/profile/alamat.js
+++ pages/dashboard/profile/alamat.js
@@ -13,74 +13,77 @@ const AlamatAdmin = () => {
                     <h4 className="text-gray-600">Alamat Admin</h4>
                     <div className="card shadow">
                         <div className="card-body">
-                            <div className="row mb-3">
-                                <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Provinsi</label>
-                                <div className="col-sm-9">
-                                    <select name="provinsi" className="form-select" aria-label="Default select example">
-                                        <option defaultValue={'Pilih Provinsi'}>-- Pilih Provinsi --</option>
-                                        <option value={1}>Jawa Barat</option>
-                                        <option value={2}>Jawa Tengah</option>
-                                        <option value={3}>Jawa Timur</option>
-                                    </select>
+
+                            <form method="POST">
+                                <div className="row mb-3">
+                                    <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Provinsi</label>
+                                    <div className="col-sm-9">
+                                        <select name="provinsi" className="form-select" aria-label="Default select example">
+                                            <option defaultValue={'Pilih Provinsi'}>-- Pilih Provinsi --</option>
+                                            <option value={1}>Jawa Barat</option>
+                                            <option value={2}>Jawa Tengah</option>
+                                            <option value={3}>Jawa Timur</option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kota</label>
-                                <div className="col-sm-9">
-                                    <select name="kota" className="form-select" aria-label="Default select example">
-                                        <option defaultValue={'Pilih Kota'}>-- Pilih Kota --</option>
-                                        <option value={1}>Jepara</option>
-                                        <option value={2}>Pati</option>
-                                        <option value={3}>Kudus</option>
-                                    </select>
+                                <div className="row mb-3">
+                                    <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kota</label>
+                                    <div className="col-sm-9">
+                                        <select name="kota" className="form-select" aria-label="Default select example">
+                                            <option defaultValue={'Pilih Kota'}>-- Pilih Kota --</option>
+                                            <option value={1}>Jepara</option>
+                                            <option value={2}>Pati</option>
+                                            <option value={3}>Kudus</option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kecamatan</label>
-                                <div className="col-sm-9">
-                                    <select name="kecamatan" className="form-select" aria-label="Default select example">
-                                        <option defaultValue={'Pilih Kecamatan'}>-- Pilih Kecamatan --</option>
-                                        <option value={1}>Kembang</option>
-                                        <option value={2}>Bangsri</option>
-                                        <option value={3}>Mlonggo</option>
-                                    </select>
+                                <div className="row mb-3">
+                                    <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kecamatan</label>
+                                    <div className="col-sm-9">
+                                        <select name="kecamatan" className="form-select" aria-label="Default select example">
+                                            <option defaultValue={'Pilih Kecamatan'}>-- Pilih Kecamatan --</option>
+                                            <option value={1}>Kembang</option>
+                                            <option value={2}>Bangsri</option>
+                                            <option value={3}>Mlonggo</option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kelurahan</label>
-                                <div className="col-sm-9">
-                                    <select name="kelurahan" className="form-select" aria-label="Default select example">
-                                        <option defaultValue={'Provinsi'}>-- Piih Kelurahan --</option>
-                                        <option value={1}>Kancilan</option>
-                                        <option value={2}>Tubanan</option>
-                                        <option value={3}>Kaliaman</option>
-                                    </select>
+                                <div className="row mb-3">
+                                    <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Kelurahan</label>
+                                    <div className="col-sm-9">
+                                        <select name="kelurahan" className="form-select" aria-label="Default select example">
+                                            <option defaultValue={'Provinsi'}>-- Piih Kelurahan --</option>
+                                            <option value={1}>Kancilan</option>
+                                            <option value={2}>Tubanan</option>
+                                            <option value={3}>Kaliaman</option>
+                                        </select>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="foto" className="col-sm-2 col-form-label">Detail Nama Jalan RT/RW</label>
-                                <div className="col-sm-9">
-                                    <textarea name="alamat" className="form-control" cols={30} rows={5} required></textarea>
+                                <div className="row mb-3">
+                                    <label htmlFor="foto" className="col-sm-2 col-form-label">Detail Nama Jalan RT/RW</label>
+                                    <div className="col-sm-9">
+                                        <textarea name="alamat" className="form-control" cols={30} rows={5} required></textarea>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="password" className="col-sm-2 col-form-label">Latitude</label>
-                                <div className="col-sm-9">
-                                    <input type="text" className="form-control" name="latitude" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="password" className="col-sm-2 col-form-label">Latitude</label>
+                                    <div className="col-sm-9">
+                                        <input type="text" className="form-control" name="latitude" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="password" className="col-sm-2 col-form-label">Longtitude </label>
-                                <div className="col-sm-9">
-                                    <input type="text" className="form-control" name="longtitude" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="password" className="col-sm-2 col-form-label">Longtitude </label>
+                                    <div className="col-sm-9">
+                                        <input type="text" className="form-control" name="longtitude" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <div className="col-sm-5">
-                                    <a href="#" className="btn btn-warning mr-3">Cancel</a>
-                                    <button type="submit" className="btn btn-success">Update</button>
+                                <div className="row mb-3">
+                                    <div className="col-sm-5">
+                                        <a href="#" className="btn btn-warning mr-3">Cancel</a>
+                                        <button type="submit" className="btn btn-success">Update</button>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
--- pages/dashboard/profile/edit_profile.js
+++ pages/dashboard/profile/edit_profile.js
@@ -13,47 +13,51 @@ const EditProfileAdmin = () => {
                     <h4 className="text-gray-600">Edit Profile</h4>
                     <div className="card shadow">
                         <div className="card-body">
-                            <div className="row mb-3">
-                                <div className="col-sm-9">
-                                    <input type="hidden" className="form-control" name="id_user" required readOnly/>
+
+                            <form method="POST">
+                                <div className="row mb-3">
+                                    <div className="col-sm-9">
+                                        <input type="hidden" className="form-control" name="id_user" required readOnly/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Nama Lengkap</label>
-                                <div className="col-sm-9">
-                                    <input type="text" className="form-control" name="nama_lengkap" placeholder="Ubah Nama" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="nama_lengkap" className="col-sm-2 col-form-label">Nama Lengkap</label>
+                                    <div className="col-sm-9">
+                                        <input type="text" className="form-control" name="nama_lengkap" placeholder="Ubah Nama" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
-                                <div className="col-sm-9">
-                                    <input type="email" className="form-control" name="email" placeholder="Ubah Email" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                                    <div className="col-sm-9">
+                                        <input type="email" className="form-control" name="email" placeholder="Ubah Email" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="no_telp" className="col-sm-2 col-form-label">No. Telephone</label>
-                                <div className="col-sm-9">
-                                    <input type="number" className="form-control" name="no_telp" placeholder="Ubah Nomor Telephone" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="no_telp" className="col-sm-2 col-form-label">No. Telephone</label>
+                                    <div className="col-sm-9">
+                                        <input type="number" className="form-control" name="no_telp" placeholder="Ubah Nomor Telephone" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="foto" className="col-sm-2 col-form-label">Foto</label>
-                                <div className="col-sm-9">
-                                    <input type="file" className="form-control" name="foto" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="foto" className="col-sm-2 col-form-label">Foto</label>
+                                    <div className="col-sm-9">
+                                        <input type="file" className="form-control" name="foto" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
-                                <div className="col-sm-9">
-                                    <input type="password" className="form-control" name="password" required/>
+                                <div className="row mb-3">
+                                    <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                                    <div className="col-sm-9">
+                                        <input type="password" className="form-control" name="password" required/>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="row mb-3">
-                                <div className="col-sm-5">
-                                    <a href="#" className="btn btn-warning mr-3">Cancel</a>
-                                    <button type="submit" className="btn btn-success">Update</button>
+                                <div className="row mb-3">
+                                    <div className="col-sm-5">
+                                        <a href="#" className="btn btn-warning mr-3">Cancel</a>
+                                        <button type="submit" className="btn btn-success">Update</button>
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
+                            
                         </div>
                     </div>
                 </div>
