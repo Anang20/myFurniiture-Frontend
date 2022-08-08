24f7a5833ea279c8c6abf6d85bda2ff72ef362d5
Anang20 <anangsyah766@gmail.com>
2022-08-08 22:44:41 +0700

    FEAT: menambahkan halaman tambah produk

--- pages/dashboard/produk/tambah_produk.js
+++ pages/dashboard/produk/tambah_produk.js
@@ -1,9 +1,89 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import axios from "axios";
 import NavbarAdmin from "../../components/navbar_admin";
 import SidebarAdmin from "../../components/sidebar_admin";
+import Head from "next/head";
+import Link from "next/link";
 
 const TambahProduk = () => {
+
+    const [nama_produk, setNamaProduk] = useState([])
+    const [gambar, setGambar] = useState([])
+    const [harga, setHarga] = useState([])
+    const [deskripsi, setDeskripsi] = useState([])
+    const [stok, setStok] = useState([])
+
+    const onChangeNamaProduk = (e) => {
+        const value = e.target.value
+        setNamaProduk(value)
+    }
+
+    const onChangeGambar = async (e) => {
+        const value = e.target.files[0] 
+        const data = {
+            file : value
+        }
+        try{
+            const res = await axios.post("http://localhost:3222/file/upload", data, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+            console.log(res, "apa itu res nya")
+            setGambar(res.data.data.filename)
+        } catch(err){
+
+        }
+    }
+
+    const onChangeHarga = (e) => {
+        const value = e.target.value
+        setHarga(value)
+    }
+
+    const onChangeDeskripsi = (e) => {
+        const value = e.target.value
+        setDeskripsi(value)
+    }
+
+    const onChangeStok = (e) => {
+        const value = e.target.value
+        setStok(value)
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+    }
+
+    const router = useRouter()
+
+    const tambahProdukSubmit = async () => {
+        try {
+            const data = {
+            'nama_produk': nama_produk,
+            'gambar': gambar,
+            'harga': harga,
+            'deskripsi': deskripsi,
+            'stok': stok,
+            }
+
+            const res = await axios.post("http://localhost:3222/produk", data, {
+                headers: {
+                    'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+
+    }catch(e){
+
+    }}
     return (
         <>
+        <Head>
+            <title>MyFuniture | Tambah Produk</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <div id="wrapper">
             <SidebarAdmin/>
             <div id="content-wrapper" className="d-flex flex-column">
@@ -14,46 +94,43 @@ const TambahProduk = () => {
                     <div className="card shadow">
                         <div className="card-body">
 
-                            <form method="POST">
-                                <div className="row mb-3">
-                                    <div className="col-sm-9">
-                                        <input type="hidden" className="form-control" name="id_produk" required readOnly/>
-                                    </div>
-                                </div>
+                            <form onSubmit={onFormSubmit} method="POST">
                                 <div className="row mb-3">
                                     <label htmlFor="nama_produk" className="col-sm-2 col-form-label">Nama Produk</label>
                                     <div className="col-sm-9">
-                                        <input type="text" className="form-control" name="nama_produk" placeholder="Masukkan Nama Produk" required/>
+                                        <input type="text" className="form-control" onChange={onChangeNamaProduk} placeholder="Masukkan Nama Produk" required/>
                                     </div>
                                 </div>
                                 <div className="row mb-3">
                                     <label htmlFor="gambar" className="col-sm-2 col-form-label">Gambar</label>
                                     <div className="col-sm-9">
-                                        <input type="file" className="form-control" name="gambar" required/>
+                                        <input type="file" className="form-control" onChange={onChangeGambar}  />
                                     </div>
                                 </div>
                                 <div className="row mb-3">
                                     <label htmlFor="harga" className="col-sm-2 col-form-label">Harga</label>
                                     <div className="col-sm-9">
-                                        <input type="number" className="form-control" name="harga" placeholder="Masukkan Harga" required/>
+                                        <input type="number" className="form-control" onChange={onChangeHarga} placeholder="Masukkan Harga" required/>
                                     </div>
                                 </div>
                                 <div className="row mb-3">
                                     <label htmlFor="deskripsi" className="col-sm-2 col-form-label">Deskripsi</label>
                                     <div className="col-sm-9">
-                                    <textarea name="deskripsi" className="form-control" cols={30} rows={5} required></textarea>
+                                    <textarea name="deskripsi" className="form-control" onChange={onChangeDeskripsi} cols={30} rows={5} required></textarea>
                                     </div>
                                 </div>
                                 <div className="row mb-3">
                                     <label htmlFor="stok" className="col-sm-2 col-form-label">Stok</label>
                                     <div className="col-sm-9">
-                                        <input type="stok" className="form-control" name="stok" required/>
+                                        <input type="stok" className="form-control" onChange={onChangeStok} name="stok" required/>
                                     </div>
                                 </div>
                                 <div className="row mb-3">
                                     <div className="col-sm-5">
-                                        <a href="#" className="btn btn-warning mr-3">Cancel</a>
-                                        <button type="submit" className="btn btn-success">Update</button>
+                                        <Link href="/dashboard/produk">
+                                            <a className="btn btn-warning mr-3">Cancel</a>
+                                        </Link>
+                                        <button type="submit" className="btn btn-success" onClick={tambahProdukSubmit}>Tambah</button>
                                     </div>
                                 </div>
                             </form>
@@ -66,5 +143,5 @@ const TambahProduk = () => {
     </div>
     </>
     )
-}
+    }
 export default TambahProduk;
