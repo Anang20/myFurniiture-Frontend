b599d0235a5583b77e6d920df6b4e1639a0ca9bb
Anang20 <anangsyah766@gmail.com>
2022-08-08 22:49:33 +0700

    FEAT: menambahkan halaman laporan transaksi

---
+++ pages/dashboard/order/index.js
@@ -0,0 +1,255 @@
+import NavbarAdmin from "../../components/navbar_admin";
+import SidebarAdmin from "../../components/sidebar_admin";
+import { DownloadOutlined, SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space, Table } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import Highlighter from 'react-highlight-words';
+import 'antd/dist/antd.css';
+import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import Head from "next/head";
+
+  const Order = () => {
+
+    const [detailProduk, setDetailProduk] = useState();
+
+    const getProduk = async () => {
+        const dataProduk = await axios.get("http://localhost:3222/produk");
+        const produk = dataProduk.data.items;
+        setDetailProduk(produk);
+    }
+    useEffect(() => {
+        getProduk()
+    }, []);
+
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+  
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      setSearchText(selectedKeys[0]);
+      setSearchedColumn(dataIndex);
+    };
+  
+    const handleReset = (clearFilters) => {
+      clearFilters();
+      setSearchText('');
+    };
+  
+    const getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div
+          style={{
+            padding: 8,
+          }}
+        >
+          <Input
+            ref={searchInput}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{
+              marginBottom: 8,
+              display: 'block',
+            }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+              icon={<SearchOutlined />}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => clearFilters && handleReset(clearFilters)}
+              size="small"
+              style={{
+                width: 90,
+              }}
+            >
+              Reset
+            </Button>
+            <Button
+              type="link"
+              size="small"
+              onClick={() => {
+                confirm({
+                  closeDropdown: false,
+                });
+                setSearchText(selectedKeys[0]);
+                setSearchedColumn(dataIndex);
+              }}
+            >
+              Filter
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined
+          style={{
+            color: filtered ? '#1890ff' : undefined,
+          }}
+        />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+      onFilterDropdownVisibleChange: (visible) => {
+        if (visible) {
+          setTimeout(() => searchInput.current?.select(), 100);
+        }
+      },
+      render: (text) =>
+        searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{
+              backgroundColor: '#ffc069',
+              padding: 0,
+            }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+  
+    const columns = [
+      {
+        title: 'no',
+        dataIndex: 'nomor',
+        value: 'no',
+        width: 10,
+        fixed: 'left',
+        sorter: (a, b) => a.no.length - b.no.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Tanggal',
+        dataIndex: 'tanggal',
+        value: 'created_at',
+        width: 20,
+        fixed: 'left',
+        ...getColumnSearchProps('tanggal'),
+      },
+      {
+        title: 'Nama',
+        dataIndex: 'nama',
+        value: 'nama_lengkap',
+        width: 20,
+        ...getColumnSearchProps('nama'),
+      },
+      {
+        title: 'Produk',
+        dataIndex: 'nama_produk',
+        value: 'nama_produk',
+        width: 20,
+        sorter: (a, b) => a.nama_produk.length - b.nama_produk.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Kuantiti',
+        dataIndex: 'quantity',
+        value: 'quantity',
+        width: 10,
+        ...getColumnSearchProps('quantity'),
+      },
+      {
+        title: 'Harga Barang',
+        dataIndex: 'harga_barang',
+        value: 'harga_barang',
+        width: 20,
+        ...getColumnSearchProps('harga_barang'),
+      },
+      {
+        title: 'Harga Kirim',
+        dataIndex: 'harga_kirim',
+        value: 'harga_kirim',
+        width: 20,
+        ...getColumnSearchProps('harga_kirim'),
+      },
+      {
+        title: 'Total Order',
+        dataIndex: 'total_order',
+        value: 'total_order',
+        width: 20,
+        sorter: (a, b) => a.total_order.length - b.total_order.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Alamat',
+        dataIndex: 'alamat',
+        value: 'alamat',
+        width: 20,
+        sorter: (a, b) => a.alamat.length - b.alamat.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Status',
+        dataIndex: 'status',
+        value: 'status',
+        width: 20,
+        render: () => (
+            <>
+            <span class='badge badge-pill badge-primary' style={{ backgroundColor: '#0D6EFD', }}>Sudah Bayar</span>
+            </>
+        ),
+        sorter: (a, b) => a.status.length - b.status.length,
+        sortDirections: ['descend', 'ascend'],
+      },
+      {
+        title: 'Action',
+        key: 'operation',
+        width: 20,
+        render: () => (
+          <>
+          <button className="btn btn-sm btn-success shadow-sm me-3">Kirim Barang</button>
+          </>
+        ),
+      },
+    ]
+    
+    return ( 
+      <>
+      <Head>
+        <title>MyFuniture | Order</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+       <div id="wrapper" style={{ maxWidth: 1120 }}>
+            <SidebarAdmin/>
+            <div id="content-wrapper" className="d-flex flex-column">
+                <div id="content">
+                  <NavbarAdmin/>
+                  <div className="container-fluid">
+                      <h4 className="text-gray-600">Order</h4>
+                      <div className="card shadow">
+                          <div className="card-body">
+                          <Table
+                          columns={columns}
+                          dataSource={detailProduk}
+                          scroll={{
+                            x: 1500,
+                            y: 300,
+                          }}
+                          />
+                          </div>
+                      </div>  
+                  </div>
+                </div>
+            </div>
+        </div>
+      </>
+    )
+  };
+  
+  export default Order ;
