2821b400a20968ef176129b9b9f22d953da9a82d
Anang20 <anangsyah766@gmail.com>
2022-08-10 19:53:53 +0700

    FIX: menambahkan config

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import { useEffect } from "react";
 import '../styles/globals.css';
+import 'antd/dist/antd.css';
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import axios from "axios";
 config.autoAddCss = false;
+
+axios.defaults.validateStatus = (status) => status < 500
 
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
